cc077076ee7759a3ef638f45f0dcdc5e10b0c2a0
pedroreal12 <pedrofilipereal12@gmail.com>
2022-12-14 18:33:11 +0000

    mudei os comentarios de # para //

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#define TAM 25
+//include <stdio.h>
+//include <string.h>
+//include <stdlib.h>
+//define TAM 25
 
-#se table == 1 significa que a celula esta viva / se table == 0 a celula esta morta
+//se table == 1 significa que a celula esta viva / se table == 0 a celula esta morta
 
-#table (tabuleiro) como variavel global para ser mais facil de se acessar fora do main e etc.
+//table (tabuleiro) como variavel global para ser mais facil de se acessar fora do main e etc.
 int table[TAM][TAM];
 
 void resetTable(){
@@ -16,23 +16,23 @@ void resetTable(){
 	}
 }
 
-#Adiciona apenas 1 celula, os parametros sao as coordenadas inseridas pelo utilizador
-#TODO: If's para lidar com erros
+//Adiciona apenas 1 celula, os parametros sao as coordenadas inseridas pelo utilizador
+//TODO: If's para lidar com erros
 void addOneCell(int line, int column){
 	table[line][column] = 1;
 	
 }
 
-#Apaga apenas 1 celula, os parametros sao as coordenadas inseridas pelo utilizador
-#TODO: If's para lidar com erros
+//Apaga apenas 1 celula, os parametros sao as coordenadas inseridas pelo utilizador
+//TODO: If's para lidar com erros
 void deleteOneCell(int line, int column){
 	table[line][column] = 0;
 	
 }
 
-#Adiciona Varias Celulas
-#quant_cells == Quantidade de Celulas a inserir pelo utilizador
-#TODO: If's para lidar com erros
+//Adiciona Varias Celulas
+//quant_cells == Quantidade de Celulas a inserir pelo utilizador
+//TODO: If's para lidar com erros
 void addCells(int quant_cells){
 	int line, column;
 	for (int i = 1; i <= quant_cells; i++){
@@ -46,9 +46,9 @@ void addCells(int quant_cells){
 	}
 }
 
-#Apaga Varias Celulas
-#quant_cells == Quantidade de Celulas a inserir pelo utilizador
-#TODO: If's para lidar com erros
+//Apaga Varias Celulas
+//quant_cells == Quantidade de Celulas a inserir pelo utilizador
+///TODO: If's para lidar com erros
 void deleteCells(int quant_cells){}
 	int line, column;
 	for (int i = 1; i <= quant_cells; i++){
@@ -62,19 +62,19 @@ void deleteCells(int quant_cells){}
 	}
 }
 
-#Avanca 1 geracao
-#Talvez de para fazer esta funcao com recursao e assim avancar nao precisar da advanceGens
+//Avanca 1 geracao
+//Talvez de para fazer esta funcao com recursao e assim avancar nao precisar da advanceGens
 void advanceOneGen(){
-	#TODO
+	//TODO
 }
 
-#Avanca Varias geracoes (Inseridas pelo utilizador com quant_gens)
+//Avanca Varias geracoes (Inseridas pelo utilizador com quant_gens)
 void advanceGens(int quant_gens){
-	#TODO
+	//TODO
 }
 
-#Funcoes para as Estatisticas do jogo
-#Celulas vivas na presente geracao
+//Funcoes para as Estatisticas do jogo
+//Celulas vivas na presente geracao
 int aliveCells(){
 	int contAliveCells = 0;
 	for (int line = 0; line < TAM; line++) {
