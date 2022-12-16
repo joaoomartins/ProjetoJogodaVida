fbd9b22e5f3699600fb0db58f1972b7a8ac365ca
joaoomartins <joao62806@gmail.com>
2022-12-16 13:59:13 +0000

    function menu, function tabuleiro, e o case que ja funfa.

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-void menu() {
+int menu() {
+    int option;
 	printf("*-------------------------------------------------------------------------------------*\n");
     printf("|       _                                 _            __      __  _       _          |\n");
     printf("|      | |                               | |           \\ \\    / / (_)     | |         |\n");
@@ -16,25 +17,79 @@ void menu() {
     printf("|*************************************************************************************|\n");
     printf("|******************************** MENU PRINCIPAL *************************************|\n");
     printf("|************************************ Opcoes *****************************************|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|**                                                                                 **|\n");
-    printf("|*----------------------------------------------------------------------------------**|\n ");
+    printf("|*************************************************************************************|\n");
+    printf("|**      {1} - Inserir celula           **       {6} - Avancar uma geracao          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**      {2} - Apagar celula            **       {7} - Avancar n geracoes           **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**      {3} - Inserir varias celulas   **       {8} - Estatisticas                 **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**      {4} - Apagar varias celulas    **       {9} - Configuracoes                **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**      {5} - Apagar o tabuleiro       **       {0} - Sair                         **|\n");
+    printf("*-------------------------------------------------------------------------------------*\n");
+
+    printf("Escolha uma opcao:");
+    scanf("%d", &option);
+    fflush(stdin);
+    return option;
+}
+
+void tabuleiro() {
+
+    int tabuleiro[25][25];
+
+    for (int i = 0; i <= 25 ; i++)
+    {
+    	printf("\n");
+       for (int j = 0; j <= 25; j++)
+       {
+        printf(". ", tabuleiro[i,j]);
+       }
+    }
+    system("pause");
 }
 
 int main () {
-    menu();
+    int option;
+    //tabuleiro();
+    while (option != 0)
+    {
+        option = menu();
+         switch (option)
+        {
+        case 0:
+            break;
+        case 1:
+            tabuleiro();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            tabuleiro();
+            break;
+        case 9:
+            break;                      
+        default:
+            printf("Opcao invalida, introduza uma opcao valida!");
+            system("pause");
+            menu();
+            break;
+        }
+    } 
 	return 0;
 }
