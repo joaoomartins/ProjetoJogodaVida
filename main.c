12f770dff94c13ca111aaf9de066bc193387410a
pedroreal12 <pedrofilipereal12@gmail.com>
2022-12-29 17:35:20 +0000

    adicionei novas funcionalidades para as configuracoes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 #define TAM 25
 
 //se table == 1 significa que a celula esta viva / se table == 0 a celula esta morta
@@ -18,7 +19,13 @@ youngestCell = 0,
 genYoungestCellBorn = 0,
 genOldestCellBorn = 0,
 lessCellsAlive = 0,
-genLessCellsAlive = 0;
+genLessCellsAlive = 0,
+TAMColumnsConfigs = TAM,
+TAMLinesConfigs = TAM,
+showGameStatisticsAdvanceGen = 0,
+showTableAdvanceGen = 1;
+
+float secsPerGen = 0.5;
 
 //Estas funcoes sao chamadas no case mas nao estao no menu, sao apenas para facilitar os testes
 void addGlider() {
@@ -70,10 +77,20 @@ void addBote() {
 	mostCellsLived = 5;
 }
 
+void cooldown(float seconds) {
+	clock_t start = clock();
+	clock_t period = seconds * CLOCKS_PER_SEC;
+	clock_t elapsed;
+	
+	do {
+		elapsed = clock() - start;
+	} while (elapsed < period);
+}
+
 void resetTable(){
 	int line, column;
-	for (int line = 0; line < TAM; line++){
-		for (int column = 0; column < TAM; column++){
+	for (int line = 0; line < TAMLinesConfigs; line++){
+		for (int column = 0; column < TAMColumnsConfigs; column++){
 			table[line][column] = 0;
 			cellsToBorn[line][column] = 0;
 			cellsToKill[line][column] = 0;
@@ -87,6 +104,9 @@ void resetTable(){
 	youngestCell = 0;
 	genYoungestCellBorn = 0;
 	genOldestCellBorn = 0;
+	TAMColumnsConfigs = TAM;
+	TAMLinesConfigs = TAM;
+	secsPerGen = 0.5;
 }
 
 void showTable(){
@@ -94,8 +114,8 @@ void showTable(){
 	
 	system("cls");
 	
-	for (line = 0; line < TAM; line++) {
-		for (column = 0; column < TAM; column++) {
+	for (line = 0; line < TAMLinesConfigs; line++) {
+		for (column = 0; column < TAMColumnsConfigs; column++) {
 			if (table[line][column] == 1) {
 				printf(" * ");
 			} else {
@@ -189,8 +209,8 @@ void deleteCells(){
 //Celulas vivas na presente geracao
 int aliveCells(){
 	int countAliveCells = 0, line, column;;
-	for (int line = 0; line < TAM; line++) {
-		for (int column = 0; column < TAM; column++) {
+	for (int line = 0; line < TAMLinesConfigs; line++) {
+		for (int column = 0; column < TAMColumnsConfigs; column++) {
 			if (table[line][column] == 1) {
 				countAliveCells += 1;
 			}
@@ -199,6 +219,81 @@ int aliveCells(){
 	}
 	
 	return countAliveCells;
+}
+
+void changeTableTAM() {
+	int TAMLines, TAMColumns;
+	printf("\nALERTA: Por limitacoes tecnicas o tamanho MAXIMO do tabuleiro e 25x25.");
+	printf("\nTamanho atual do tabuleiro: %dx%d", TAMLinesConfigs, TAMColumnsConfigs);
+	printf("\nIntroduza o tamanho do tabuleiro (Exemplo: 25x25): ");
+	scanf("%dx%d", &TAMLines, &TAMColumns);
+	if (TAM - TAMLines > 0 && TAM - TAMColumns > 0) {
+		TAMLinesConfigs = TAMLines;
+		TAMColumnsConfigs = TAMColumns;
+		printf("\nTamanho do tabuleiro atualizado com sucesso!\n");
+		cooldown(3.0);
+	} else {
+		printf("\nLimite de tabuleiro excedido. Por favor insira um numero menor que 25 no tamanho do tabuleiro.");
+	}
+}
+
+void changeTableVelocity() {
+	float newSecs = 0;
+	printf("\nIntroduza a quantidade de tempo que deseja que o programa demore ao avancar de geracao em segundos (Exemplo 1): ");
+	scanf("%f", &newSecs);
+	
+	if (newSecs >= 0) {
+		secsPerGen = newSecs;
+		printf("\nTempo atualizado com sucesso!\n");
+		cooldown(3);
+	} else {
+		printf("\nPor favor introduza um valor valido e em segundos!");
+	}
+}
+//ED para Enable/Disable. Esta funcao serve para mostrar/nao mostrar o tabuleiro quando se avanca de geracao
+//Por pre-definicao o tabuleiro e mostrado, o utilizador pode, ativar ou desativar essa funcionalidade.
+void EDTableOnAdvanceGen() {
+	if (showTableAdvanceGen == 1) {
+		showTableAdvanceGen = 0;
+		printf("\nO tabuleiro nao ira aparecer ao avancar a geracao!\n");
+		cooldown(3.0);
+	} else {
+		showTableAdvanceGen = 1;
+		printf("\nO tabuleiro ira aparecer ao avancar a geracao!\n");
+		cooldown(3.0);
+	}
+}
+
+void EDStatisticsOnAdvanceGen() {
+	if (showGameStatisticsAdvanceGen == 1) {
+		showGameStatisticsAdvanceGen = 0;
+		printf("\nAs estatisticas do jogo nao iram aparecer ao avancar a geracao!\n");
+		cooldown(3.0);
+	} else {
+		showGameStatisticsAdvanceGen = 1;
+		printf("\nAs estatisticas do jogo iram aparecer ao avancar a geracao!\n");
+		cooldown(3.0);
+	}
+}
+
+void getGameStatistics() {
+	printf("\n");
+	printf("\nQuantidade de celulas vivas: %d", aliveCells());
+	printf("\nNumero maximo de celulas vivas %d; Geracao: %d", mostCellsLived, genMostCellsAlive);
+	printf("\nNumero minimo de celulas vivas %d; Geracao: %d", lessCellsAlive, genLessCellsAlive);
+	printf("\nIdade da celula mais velha %d; Nasceu na geracao: %d", oldestCell, genOldestCellBorn);
+	printf("\nIdade da celula mais nova %d; Nasceu na geracao: %d", youngestCell, genYoungestCellBorn);
+	printf("\nGeracao atual: %d", gen);
+	printf("\n");
+	printf("\nHistograma de todas as celulas vivas:");
+	for (int line = 0; line < TAMLinesConfigs; line++) {
+		for (int column = 0; column < TAMColumnsConfigs; column++) {
+			if (table[line][column] == 1) {
+				printf("\nIdade da celula (%d, %d): %d; \nNasceu na geracao: %d", line, column, ages[line][column], gen - ages[line][column]);
+			}
+		}
+	}
+	printf("\n");
 }
 
 //neighbours: Celulas vizinhas vivas
@@ -210,11 +305,18 @@ int advanceGen(int quant_gens){
 	if (quant_gens == 0) {
 		return 0;
 	} else {
-		showTable();
-		system("pause");
 		
-		for (line = 0; line < TAM; line++) {
-			for (column = 0; column < TAM; column++) {
+	if (showTableAdvanceGen == 1 && gen > 0) {
+		showTable();
+		if (showGameStatisticsAdvanceGen == 1) {
+			getGameStatistics();
+			cooldown(2.5);
+		}
+		cooldown(secsPerGen);
+	}
+		
+		for (line = 0; line < TAMLinesConfigs; line++) {
+			for (column = 0; column < TAMColumnsConfigs; column++) {
 				
 				if (line == 0 && column == 0) {
 					if (table[line + 1][column] == 1) {
@@ -229,7 +331,7 @@ int advanceGen(int quant_gens){
 						neighbours += 1;
 					}
 					
-				} else if (line == 0 && column > 0 && column < TAM) {
+				} else if (line == 0 && column > 0 && column < TAMColumnsConfigs) {
 					if (table[line + 1][column] == 1) {
 						neighbours += 1;
 					}
@@ -250,7 +352,7 @@ int advanceGen(int quant_gens){
 						neighbours += 1;
 					}
 					
-				} else if (line == TAM - 1 && column == TAM - 1) {
+				} else if (line == TAMLinesConfigs - 1 && column == TAMColumnsConfigs - 1) {
 					if (table[line - 1][column] == 1) {
 						neighbours += 1;
 					}
@@ -264,7 +366,7 @@ int advanceGen(int quant_gens){
 					}
 					
 					
-				} else if (line == TAM - 1 && column < TAM && column >= 0) {
+				} else if (line == TAMLinesConfigs - 1 && column < TAMColumnsConfigs && column >= 0) {
 					if (table[line - 1][column] == 1) {
 						neighbours += 1;
 					}
@@ -333,8 +435,8 @@ int advanceGen(int quant_gens){
 			}
 		}
 	// Se o valor de cellsToBorn == 1 Significa que uma nova celula vai nascer
-	for (line = 0; line < TAM; line++) {
-		for (column = 0; column < TAM; column++) {
+	for (line = 0; line < TAMLinesConfigs; line++) {
+		for (column = 0; column < TAMColumnsConfigs; column++) {
 			if (cellsToBorn[line][column] == 1) {
 				table[line][column] = 1;
 				cellsToBorn[line][column] = 0;
@@ -369,8 +471,8 @@ int advanceGen(int quant_gens){
 		genMostCellsAlive = gen + 1;
 	}
 	
-	for (int line = 0; line < TAM; line++) {
-		for (column = 0; column < TAM; column++) {
+	for (int line = 0; line < TAMLinesConfigs; line++) {
+		for (column = 0; column < TAMColumnsConfigs; column++) {
 			if (table[line][column] == 1) {
 				ages[line][column] += 1;
 			}
@@ -434,35 +536,47 @@ int menu() {
     return option;
 }
 
-void getGameStatistics() {
-	printf("\n");
-	printf("\nQuantidade de celulas vivas: %d", aliveCells());
-	printf("\nNumero maximo de celulas vivas %d; Geracao: %d", mostCellsLived, genMostCellsAlive);
-	printf("\nNumero minimo de celulas vivas %d; Geracao: %d", lessCellsAlive, genLessCellsAlive);
-	printf("\nIdade da celula mais velha %d; Nasceu na geracao: %d", oldestCell, genOldestCellBorn);
-	printf("\nIdade da celula mais nova %d; Nasceu na geracao: %d", youngestCell, genYoungestCellBorn);
-	printf("\nGeracao atual: %d", gen);
-	printf("\n");
-	printf("\nHistograma de todas as celulas vivas:");
-	for (int line = 0; line < TAM; line++) {
-		for (int column = 0; column < TAM; column++) {
-			if (table[line][column] == 1) {
-				printf("\nIdade da celula (%d, %d): %d; \nNasceu na geracao: %d", line, column, ages[line][column], gen - ages[line][column]);
-			}
-		}
-	}
-	
-	printf("\n");
-	system("pause");
+int menuConfigs() {
+    int option;
+    system("cls");
+	printf("*-------------------------------------------------------------------------------------*\n");
+    printf("|       _                                 _            __      __  _       _          |\n");
+    printf("|      | |                               | |           \\ \\    / / (_)     | |         |\n");
+    printf("|      | |   ___     __ _    ___       __| |   __ _     \\ \\  / /   _    __| |   __ _  |\n");
+    printf("|  _   | |  / _ \\   / _` |  / _ \\     / _` |  / _` |     \\ \\/ /   | |  / _` |  / _` | |\n");
+    printf("| | |__| | | (_) | | (_| | | (_) |   | (_| | | (_| |      \\  /    | | | (_| | | (_| | |\n");
+    printf("|  \\____/   \\___/   \\__, |  \\___/     \\__,_|  \\__,_|       \\/     |_|  \\__,_|  \\__,_| |\n");
+    printf("|                    __/ |                                                            |\n");
+    printf("|                   |___/                                                             |\n");
+    printf("|                                                                                     |\n");
+    printf("|*************************************************************************************|\n");
+    printf("|******************************** MENU CONFIGURACOES *********************************|\n");
+    printf("|************************************ Opcoes *****************************************|\n");
+    printf("|*************************************************************************************|\n");
+    printf("|**      {1} - Dimensoes do tabuleiro   **       {3} - Exibicao do tabuleiro        **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**      {2} - Velocidade de simulacao  **       {4} - Exibicao das estatisticas    **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                       {0} - Voltar ao menu principal                            **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("|**                                     **                                          **|\n");
+    printf("*-------------------------------------------------------------------------------------*\n");
+
+    printf("Escolha uma opcao:");
+    scanf("%d", &option);
+    fflush(stdin);
+    return option;
 }
 
 int main () {
-    int option = 10, quant_gens;
+    int option = 10, quant_gens, optionConfigs = 10;
     
     resetTable();
     
     while (option != 0)
-    {    	
+    {
     	showTable();
         
 		option = menu();
@@ -497,8 +611,33 @@ int main () {
             	break;
         	case 8:
         		getGameStatistics();
+				system("pause");
             	break;
         	case 9:
+        		while (optionConfigs != 0) {
+        			optionConfigs = menuConfigs();
+	        		switch (optionConfigs) {
+	        			case 0:
+	        				break;
+	        			case 1:
+	        				changeTableTAM();
+	        				break;
+	        			case 2:
+	        				changeTableVelocity();
+	        				break;
+	        			case 3:
+	        				EDTableOnAdvanceGen();
+	        				break;
+	        			case 4:
+	        				EDStatisticsOnAdvanceGen();
+	        				break;
+	        			default:
+	        				printf("\nOpcao invalida, introduza uma opcao valida!");
+	        				system("pause");
+	        				break;
+					}
+				}
+				optionConfigs = 10;
             	break;
             	
 			//Padroes para testes
